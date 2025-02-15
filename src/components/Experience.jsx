25e04b7bcd69f11ef889abea3c1a6f2a98df9c8d
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-02-15 17:51:29 +0530

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,20 @@
+import { Center, Environment, OrbitControls } from '@react-three/drei'
+import React from 'react'
+import { HackingDevice } from './HackingDevice'
+
+const Experience = () => {
+  return (
+    <>
+      <group rotation={[0, 0, Math.PI * -0.1]}>
+        <Center scale={0.3} position={[0, 2, 0]} rotation={[0, -Math.PI * 0.2, 0]}>
+          <HackingDevice />
+        </Center>
+      </group>
+      <Environment preset='warehouse' />
+
+      <OrbitControls />
+    </>
+  )
+}
+
+export default Experience
