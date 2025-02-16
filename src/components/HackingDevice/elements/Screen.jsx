78f7b53fba3fec5426407fb428bd1ab00fe367e5
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-02-16 11:59:37 +0530

    make it look like glass

--- src/components/HackingDevice/elements/Screen.jsx
+++ src/components/HackingDevice/elements/Screen.jsx
@@ -10,8 +10,41 @@ const Screen = () => {
 
    return (
       <group position={[-1.244, 13.249, 2.225]} scale={[1.003, 1.011, 1.003]}>
-         <mesh geometry={nodes.screen.geometry}/>
-         <Text position={[0, 0, 0.1]} color='black'>{text}</Text>
+         <mesh geometry={nodes.screen.geometry}>
+            <meshPhysicalMaterial
+               metalness={0.9}
+               roughness={0.05}
+               envMapIntensity={0.9}
+               clearcoat={1}
+               transparent={true}
+               // transmission={0.95}
+               opacity={0.5}
+               reflectivity={0.2}
+               reflectivity={0.2}
+               refractionRatio={0.985}
+               ior={0.9}
+               // side={THREE.BackSide}
+               color='green'
+            />
+         </mesh>
+         <Text position={[0, 0, -0.1]} color='black'>
+            {text}
+            <meshPhysicalMaterial
+               metalness={0.9}
+               roughness={0.05}
+               envMapIntensity={0.9}
+               clearcoat={1}
+               transparent={true}
+               // transmission={0.95}
+               opacity={0.5}
+               reflectivity={0.2}
+               reflectivity={0.2}
+               refractionRatio={0.985}
+               ior={0.9}
+               // side={THREE.BackSide}
+               color='green'
+            />
+         </Text>
       </group>
    )
 }
