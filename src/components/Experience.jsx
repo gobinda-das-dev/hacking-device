8ed2e3fcbd27ee6a0561279bc9569d9a2ed9980c
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-02-16 08:54:29 +0530

    add events when pressed move them back-words

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,20 @@
 import { Center, Environment, OrbitControls } from '@react-three/drei'
 import React, { Suspense } from 'react'
 import { HackingDevice } from './HackingDevice/HackingDevice'
+import Events from './HackingDevice/Events'
 
 const Experience = () => {
+
   return (
     <>
-      <group rotation={[0, 0, Math.PI * -0.1]}>
-        <Center scale={0.3} position={[0, 2, 0]} rotation={[0, -Math.PI * 0.2, 0]}>
-          <HackingDevice />
-        </Center>
-      </group>
+      <Events>
+        <group rotation={[0, 0, Math.PI * -0.1]}>
+          <Center scale={0.3} position={[0, 2, 0]} rotation={[0, -Math.PI * 0.2, 0]}>
+            <HackingDevice />
+          </Center>
+        </group>
+      </Events>
+      
       <Environment preset='warehouse' />
 
       <OrbitControls />
---
+++ src/components/HackingDevice/Events.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+
+const Events = ({children}) => {
+   const parent = useRef(null)
+
+   useEffect(() => {
+      const namedElements = [];
+      
+      parent.current.traverse(t => {
+         if(t.name) {
+            namedElements.push(t);
+         }
+      })
+      
+      window.addEventListener('keydown', (event) => {
+         const button = namedElements.find(b => b.name == event.key);
+
+         if(button) {
+            button.position.z = 2;
+         }
+      })
+
+      window.addEventListener('keyup', (event) => {
+         const button = namedElements.find(b => b.name == event.key);
+
+         if(button) {
+            button.position.z = 2.459;
+         }
+      })
+   }, [])
+
+   return (
+      <group ref={parent}>
+         {children}
+      </group>
+   )
+}
+
+export default Events
