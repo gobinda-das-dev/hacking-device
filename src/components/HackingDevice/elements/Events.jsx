a6164a1c5a8ae46e8f36cb032537fa08eb8bacd7
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-02-16 11:01:58 +0530

    key should pressed once

--- src/components/HackingDevice/elements/Events.jsx
+++ src/components/HackingDevice/elements/Events.jsx
@@ -1,46 +1,53 @@
-import React, { useEffect, useRef } from 'react'
-import {Howl, Howler} from 'howler';
+import React, { useEffect, useRef } from 'react';
+import { Howl, Howler } from 'howler';
 import { downAudio, upAudio } from './Audio';
 
-const Events = ({children}) => {
+const Events = ({ children }) => {
    const parent = useRef(null);
+   const keysPressed = useRef({});
 
    useEffect(() => {
       const namedElements = [];
       parent.current.traverse(t => t.name && namedElements.push(t));
 
       const down = (event) => {
+         if (keysPressed.current[event.key]) return;
+
+         keysPressed.current[event.key] = true;
+
          const button = namedElements.find(b => b.name === event.key);
 
-         if(button) {
+         if (button) {
             downAudio.play();
             button.position.z = 2;
          }
       };
 
       const up = (event) => {
+         keysPressed.current[event.key] = false;
+
          const button = namedElements.find(b => b.name === event.key);
 
-         if(button) {
+         if (button) {
             upAudio.play();
             button.position.z = 2.459;
          }
       };
-      
+
       window.addEventListener('keydown', down);
       window.addEventListener('keyup', up);
 
       return () => {
          window.removeEventListener('keydown', down);
          window.removeEventListener('keyup', up);
-      }
-   }, [])
+      };
+   }, []);
 
    return (
       <group ref={parent}>
          {children}
       </group>
-   )
-}
+   );
+};
 
-export default Events
+export default Events;
