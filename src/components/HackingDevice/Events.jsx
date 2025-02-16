9ce8398c34c1e360fad48f6e74b05c5c58233539
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-02-16 10:14:24 +0530

    play audio when click

--- src/components/HackingDevice/Events.jsx
+++ src/components/HackingDevice/Events.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useRef } from 'react'
+import {Howl, Howler} from 'howler';
 
 const Events = ({children}) => {
-   const parent = useRef(null)
+   const parent = useRef(null);
+   const downAudio = new Howl({ src: '/audio/backspace/press.mp3' });
+   const upAudio = new Howl({ src: '/audio/backspace/up.mp3' });
 
    useEffect(() => {
       const namedElements = [];
-      
-      parent.current.traverse(t => {
-         if(t.name) {
-            namedElements.push(t);
-         }
-      })
+      parent.current.traverse(t => t.name && namedElements.push(t));
       
       window.addEventListener('keydown', (event) => {
          const button = namedElements.find(b => b.name === event.key);
 
 
          if(button) {
+            downAudio.play();
             button.position.z = 2;
          }
       })
@@ -25,6 +24,7 @@ const Events = ({children}) => {
          const button = namedElements.find(b => b.name === event.key);
 
          if(button) {
+            upAudio.play();
             button.position.z = 2.459;
          }
       })
